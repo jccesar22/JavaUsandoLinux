6b9d66efe822465d5f294870bfd898a4862910b5
jcsoares22 <juliocesarwassoares931@gmail.com>
2021-09-09 06:41:15 -0300

    commit
    terminando a validação quando entra com string em um campo double

--- atividade1_teste/src/application/Program.java
+++ atividade1_teste/src/application/Program.java
@@ -8,23 +8,30 @@ import equacao.EquacaoSegundoGrau;
 public class Program {
 
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
-		 Scanner in = new Scanner(System.in);
-		 EquacaoSegundoGrau equacao = new EquacaoSegundoGrau();
-		 	try {
-	        double a, b, c;
-	        System.out.println("a");
-	            a = in.nextDouble();
-	            equacao.setA(a);
-	            System.out.println("b");
-	            b = in.nextDouble();
-	            System.out.println("c");
-	            c = in.nextDouble();
-	       
-	       equacao.Equacao(a, b, c);
-		 	}catch(InputMismatchException e) {
-		 		System.out.println("Erro: Não poder ser usado <String> no campo de <Double>");
-		 	}
-		 	in.close();
-}
+		EquacaoSegundoGrau equacao = new EquacaoSegundoGrau();
+		double a, b, c;
+		Scanner in = new Scanner(System.in);
+		try {
+			
+			System.out.println("a");
+			a = in.nextDouble();
+			System.out.println("b");
+			b = in.nextDouble();
+			System.out.println("c");
+			c = in.nextDouble();
+			equacao.Equacao(a, b, c);
+		} catch (InputMismatchException e) {// validacao dde Strinf
+			System.out.println("Erro: Não poder ser usado <String> no campo de <Double>");
+			try {
+			equacao.dados();
+			}
+			catch (InputMismatchException ev)
+			{
+				equacao.dados();
+
+			}
+		}
+		in.close();
+
 	}
+}
--- atividade1_teste/src/equacao/EquacaoSegundoGrau.java
+++ atividade1_teste/src/equacao/EquacaoSegundoGrau.java
@@ -1,12 +1,13 @@
 package equacao;
 
-import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class EquacaoSegundoGrau {
 	private double a;
 	private double b;
 	private double c;
+	Scanner in = new Scanner(System.in);
 	float delta, // delta
 			sqrtdelta, // raiz quadrada de delta
 			raiz1, raiz2; // raízes
@@ -45,25 +46,45 @@ public class EquacaoSegundoGrau {
 		this.c = c;
 	}
 
-	public void Equacao(double a, double b, double c) {
+	public void Equacao(double a, double b, double c) throws InputMismatchException {
 		try {
-			if (a != 0) {
+			 
+			if (a != 0 && b != 0 && c != 0) {
 				// sempre o "A" não pode ser zero
 				delta = (float) ((b * b) - (4 * a * c));
 				sqrtdelta = (float) Math.sqrt(delta);
 				if (delta < 0) {// para poder corrigir o NaN coloquei o delta < 0 para evitar esse problema
 					System.out.println("Delta é negativo, logo x não pertence aos numeros reais!");
+					System.out.println("delta negativo. Como delta é menor que zero, a equação não terá raízes reais, pois não existe raiz quadrada de número negativo.");
 				}
-				// se a raiz de delta for maior que 0, as raízes são reais
 				else {
+					System.out.println("O valo de DELTA: "+ delta + "\nA Raiz de delta é: "+ sqrtdelta);
 					raiz1 = (float) (((-1) * b + sqrtdelta) / (2 * a));
 					raiz2 = (float) (((-1) * b - sqrtdelta) / (2 * a));
 					System.out.println("Raízes1:" + raiz1 + " e Raízes2: " + raiz2);
 				}
 
+			}else {
+				System.out.println("O valor de <A e B e C> não pode ser Zero");
 			}
 		} catch (Exception e) {
 			System.out.println("Por Favor Digite Valores Validos!!!");
 		}
 	}
+	public void dados() {
+		try {
+			double a, b, c;
+			System.out.println("a");
+			a = in.nextDouble();
+			System.out.println("b");
+			b = in.nextDouble();
+			System.out.println("c");
+			c = in.nextDouble();
+			Equacao(a, b, c);
+		} catch (InputMismatchException e) {// validacao dde Strinf
+			System.out.println("Erro: Não poder ser usado <String> no campo de <Double>");
+		}
+		in.close();
+	}
+	
 }
