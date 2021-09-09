095feed02a0701f2b60c9549cb355a04a7cfa0b2
jcsoares22 <juliocesarwassoares931@gmail.com>
2021-09-09 05:50:38 -0300

    comit
    termimando

--- atividade1_teste/src/application/Program.java
+++ atividade1_teste/src/application/Program.java
@@ -10,15 +10,17 @@ public class Program {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		 Scanner in = new Scanner(System.in);
+		 EquacaoSegundoGrau equacao = new EquacaoSegundoGrau();
 		 	try {
 	        double a, b, c;
 	        System.out.println("a");
 	            a = in.nextDouble();
+	            equacao.setA(a);
 	            System.out.println("b");
 	            b = in.nextDouble();
 	            System.out.println("c");
 	            c = in.nextDouble();
-	       EquacaoSegundoGrau equacao = new EquacaoSegundoGrau();
+	       
 	       equacao.Equacao(a, b, c);
 		 	}catch(InputMismatchException e) {
 		 		System.out.println("Erro: Não poder ser usado <String> no campo de <Double>");
