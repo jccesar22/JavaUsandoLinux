a7def5d729c88d6a9960f51f86f443956ddf6600
jcsoares22 <juliocesarwassoares931@gmail.com>
2021-09-09 05:50:08 -0300

    commit
    Atividade da faculdade 1 programação 2 no final

--- atividade1_teste/src/application/Program.java
+++ atividade1_teste/src/application/Program.java
@@ -1,15 +1,16 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import equacao.EquacaoSegundoGrau;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		 Scanner in = new Scanner(System.in);
-
+		 	try {
 	        double a, b, c;
 	        System.out.println("a");
 	            a = in.nextDouble();
@@ -19,7 +20,9 @@ public class Program {
 	            c = in.nextDouble();
 	       EquacaoSegundoGrau equacao = new EquacaoSegundoGrau();
 	       equacao.Equacao(a, b, c);
-	       
-
+		 	}catch(InputMismatchException e) {
+		 		System.out.println("Erro: Não poder ser usado <String> no campo de <Double>");
+		 	}
+		 	in.close();
 }
 	}
--- atividade1_teste/src/equacao/EquacaoSegundoGrau.java
+++ atividade1_teste/src/equacao/EquacaoSegundoGrau.java
@@ -1,53 +1,69 @@
 package equacao;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 
 public class EquacaoSegundoGrau {
 	private double a;
 	private double b;
 	private double c;
-	float  delta,        //delta
-           sqrtdelta,    //raiz quadrada de delta
-           raiz1,raiz2;  //raízes
-	
+	float delta, // delta
+			sqrtdelta, // raiz quadrada de delta
+			raiz1, raiz2; // raízes
+
 	public EquacaoSegundoGrau() {
-		
+
 	}
+
 	public EquacaoSegundoGrau(double a, double b, double c) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 	}
 
+	public double getA() {
+		return a;
+	}
 
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
 
 	public void Equacao(double a, double b, double c) {
-		
-		if(a != 0){ 
-	        //sempre o "A" não pode ser zero
-	            delta = (float) ((b*b) - (4*a*c));
-	            sqrtdelta = (float)Math.sqrt(delta);
-	        //se a raiz de delta for maior que 0, as raízes são reais    
-	            if(delta >=0){
-	                raiz1 = (float) (((-1)*b + sqrtdelta)/(2*a));
-	                raiz2 = (float) (((-1)*b - sqrtdelta)/(2*a));
-	                System.out.println("Raízes1:"+ raiz1 + " e Raízes2: "+ raiz2);
-	               System.out.println("A raiz de DELTA: " + sqrtdelta);
-	               System.out.printf("a raiz de delta for maior que 0, as raízes são reais ");
-	            }
-	            else{
-	                delta = -delta;
-	                sqrtdelta = (float)Math.sqrt(delta);
-	                delta = -delta;
-	                sqrtdelta = (float)Math.sqrt(delta);
-	                System.out.printf("Raíz 1: %.2f + i.%.2f\n", (-b)/(2*a), (sqrtdelta)/(2*a));
-	                System.out.printf("Raíz 2: %.2f - i.%.2f\n", (-b)/(2*a), (sqrtdelta)/(2*a));
-	            }
-	            
-	        }
-	        else{
-	            System.out.println("Coeficiente 'a' inválido. Não é uma equação do Segundo grau o Coeficiente zero");
-	        }
-	    }
-	
+		try {
+			if (a != 0) {
+				// sempre o "A" não pode ser zero
+				delta = (float) ((b * b) - (4 * a * c));
+				sqrtdelta = (float) Math.sqrt(delta);
+				if (delta < 0) {// para poder corrigir o NaN coloquei o delta < 0 para evitar esse problema
+					System.out.println("Delta é negativo, logo x não pertence aos numeros reais!");
+				}
+				// se a raiz de delta for maior que 0, as raízes são reais
+				else {
+					raiz1 = (float) (((-1) * b + sqrtdelta) / (2 * a));
+					raiz2 = (float) (((-1) * b - sqrtdelta) / (2 * a));
+					System.out.println("Raízes1:" + raiz1 + " e Raízes2: " + raiz2);
+				}
+
+			}
+		} catch (Exception e) {
+			System.out.println("Por Favor Digite Valores Validos!!!");
+		}
 	}
+}
